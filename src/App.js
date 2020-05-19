d41a89e64b4d4ca5d3da2f1e25a820548e3d7484
Jordan Enev <jordan@devlabs.bg>
2020-05-19 15:17:20 +0300

    Add: `react-router`.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import EmployeesView from './EmployeesView'
 
 function App() {
-  return <EmployeesView />
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <EmployeesView />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
