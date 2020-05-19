602718cc400baf5dff9d4cea8573d5766da4ad4a
Jordan Enev <jordan@devlabs.bg>
2020-05-19 17:58:14 +0300

    Add: Employee creation.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import EmployeesView from './EmployeesView'
+import EmployeeCreate from './EmployeeCreate'
 import EmployeeEdit from './EmployeeEdit'
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
       <Switch>
         <Route exact path='/'>
           <EmployeesView />
+        </Route>
+        <Route path='/employee/create'>
+          <EmployeeCreate />
         </Route>
         <Route path='/employee/:id'>
           <EmployeeEdit />
---
+++ src/EmployeeCreate.js
@@ -0,0 +1,23 @@
+import React  from 'react';
+import { API_URL } from './config';
+import EmployeeForm from './EmployeeForm'
+
+function EmployeeCreate() {
+
+  const onSubmit = data => {
+    return fetch(`${API_URL}/employee`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  };
+
+  return (
+    <EmployeeForm onSubmit={onSubmit} />
+  );
+}
+
+export default EmployeeCreate;
