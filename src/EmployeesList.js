aeb103a6e62b20e820d16fe0b332ad4c983a999a
Jordan Enev <jordan@devlabs.bg>
2020-05-20 11:46:40 +0300

    Add: Employee deletion.

--- src/EmployeesList.js
+++ src/EmployeesList.js
@@ -6,15 +6,23 @@ function EmployeesList() {
   const [employees, setEmployees] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  async function fetchEmployees() {
+    const response = await fetch(`${API_URL}/employee`);
+    const employees = await response.json();
+
+    setEmployees(employees);
+    setIsLoading(false);
+  }
+
+  async function deleteEmployee(employee) {
+    await fetch(`${API_URL}/employee/${employee.id}`, {
+      method: 'DELETE'
+    });
+
+    setEmployees(employees.filter(({ id }) => id !== employee.id));
+  }
+
   useEffect(() => {
-    async function fetchEmployees() {
-      const response = await fetch(`${API_URL}/employee`);
-      const employees = await response.json();
-
-      setEmployees(employees);
-      setIsLoading(false);
-    }
-
     fetchEmployees();
   }, []);
 
@@ -25,6 +33,7 @@ function EmployeesList() {
       { employees.map(employee => (
         <li key={employee.id}>
           <Link to={`/employee/${employee.id}`}>{ employee.employee_name }</Link>
+          <button onClick={() => deleteEmployee(employee)}>Delete</button>
         </li>
       ))}
     </ul>
